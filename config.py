137373502a6685773493dd9172dd82921a9ce307
Elgondymogand <dagsalk@gmail.com>
2024-10-12 14:19:20 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = "7908905482:AAEncnOGQvoXNORqyMyFcLDR55AOI96fhKo"
+
+    APP_ID = "22665066"
+
+    API_HASH = "92dbe89d182f72f427972d8993850130"
